7df0c7058a10f5a020a290c123becbe916e7ebaf
7244 <65ad4d@gmail.com>
2026-06-28 06:19:15 +0000

    main.cpp better benchmark

--- main.cpp
+++ main.cpp
@@ -4,28 +4,40 @@
 #include "flwb.h"
 
 #include <thread>
+#include <random>
+#include <print>
 
-#define MAX_THREADS (8)
+#define MAX_THREADS (32)
+#define MAX_ELEMENTS (1000000)
 
 struct pile_t{
-  flwb_t<uint64_t, MAX_THREADS, 100000000, 4096> flwb;
+  flwb_t<uint64_t, MAX_THREADS, MAX_ELEMENTS, 4> flwb;
+  struct alignas(std::hardware_destructive_interference_size){
+    uint64_t sum;
+  }thread_data[MAX_THREADS];
 };
 pile_t *pile;
 
 void worker(uint32_t thread_index){
-  constexpr auto loop1_limit = 10000000;
-  auto elems = (uint32_t *)__generic_mmap(loop1_limit * sizeof(uint32_t));
+  constexpr auto hard_limit = MAX_ELEMENTS / MAX_THREADS;
+  auto elems = (uint32_t *)__generic_mmap(hard_limit * sizeof(uint32_t));
   if((uintptr_t)elems > (uintptr_t)-0x1000){
     __abort();
   }
 
-  for(auto loop0 = 1; loop0--;){
-    for(auto loop1 = loop1_limit; loop1--;){
-      elems[loop1] = pile->flwb.consume_unsafe(thread_index);
+  std::mt19937 gen(std::random_device{}());
+  
+  while(1){
+    auto limit = std::uniform_int_distribution<decltype(hard_limit)>(0, hard_limit - 1)(gen);
+
+    for(auto i = limit; i--;){
+      elems[i] = pile->flwb.consume_unsafe(thread_index);
     }
-    for(auto loop1 = loop1_limit; loop1--;){
-      pile->flwb.produce_unsafe(thread_index, elems[loop1]);
+    for(auto i = limit; i--;){
+      pile->flwb.produce_unsafe(thread_index, elems[i]);
     }
+
+    __atomic_add_fetch(&pile->thread_data[thread_index].sum, limit, __ATOMIC_SEQ_CST);
   }
 }
 
@@ -40,8 +52,19 @@ int main(){
   for(auto i = MAX_THREADS; i--;){
     threads[i] = std::thread(worker, i);
   }
-  for(auto i = MAX_THREADS; i--;){
-    threads[i].join();
+
+  std::println("data_per_block is: {}", pile->flwb.data_per_block);
+
+  uint64_t sum = 0;
+  while(1){
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    uint64_t new_sum = 0;
+    for(auto i = MAX_THREADS; i--;){
+      new_sum += __atomic_load_n(&pile->thread_data[i].sum, __ATOMIC_SEQ_CST);
+    }
+    std::println("{}", new_sum - sum);
+    sum = new_sum;
   }
 
   return 0;
